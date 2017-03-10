fd837ea365a87837f16fbca4896f7ac7d73d46a5
ww <ww_1125@outlook.com>
2017-03-09 20:33:57 -0800

    Init

---
+++ js/location_data23333.js
@@ -0,0 +1,346 @@
+var UCSC_locations = {
+    L1: {Location: "E Baskin",
+        center: {lat: 37.000355, lng: -122.063163},
+        population: 0,
+        classes: {}
+    },
+
+    L2: {Location: "Soc Sci 1",
+        center: {lat: 37.002392, lng: -122.058094},
+        population: 0,
+        classes: {}
+    },
+    L3: {Location:"Soc Sci 1",
+        center: {lat: 36.998021, lng: -122.054703},
+        population:0,
+        classes: {}
+
+    },
+    L4: {Location:"N. Sci Annex",
+        center: {lat: 36.998634, lng: -122.061080},
+        population: 0,
+        classes: {}
+    },
+    L5: {Location:"PhysSciences",
+        center: {lat: 36.999689, lng: -122.061783},
+        population: 0,
+        classes: {}
+    },
+    L6: {Location:"Cowell Acad",
+        center: {lat: 36.996658, lng: -122.054161},
+        population:0,
+        classes: {}
+    },
+    L7: {Location:"Soc Sci 2",
+        center: {lat: 37.001744, lng: -122.058836},
+        population: 0,
+        classes: {}
+    },
+    L8: {Location:"Oakes Acad",
+        center: {lat: 36.989070, lng: -122.064392},
+        population: 0,
+        classes: {}
+    },
+    L9: {Location:"Digital Arts",
+        center: {lat: 36.993933, lng: -122.060574},
+        population: 0,
+        classes: {}
+    },
+    L10: {Location:"Bay Tree Conf D",
+        center: {lat: 36.997959, lng: -122.055591},
+        population: 0,
+        classes: {}
+    },
+    L11: {Location:"Merrill Acad",
+        center: {lat: 36.999566, lng: 122.053073},
+        population: 0,
+        classes: {}
+    },
+
+    l12: {Location:"Earth Marine",
+        center: {lat: 36.998226, lng: -122.059630},
+        population:0,
+        classes: {}
+    },
+
+    l13: {Location:"Engineer 2",
+        center: {lat: 37.000916, lng: -122.063482},
+        population: 0,
+        classes: {}
+    },
+
+    l14: {Location:"McHenry Clrm",
+        center: {lat: 36.995989, lng: -122.058894},
+        population: 0,
+        classes: {}
+    },
+    l15: {Location:"Commun Bldg",
+        center: {lat: 37.001110, lng: -122.061436},
+        population: 0,
+        classes: {}
+    },
+    l16:{Location:"Porter Acad",
+        center: {lat: 36.995972, lng: -122.064801},
+        population:0,
+        classes: {}
+    },
+    l17: {Location:"Hum & Soc Sci",
+        center: {lat: 36.998427, lng: -122.054865},
+        population: 0,
+        classes: {}
+    },
+    l18: {Location:"Kresge Rec",
+        center: {lat: 36.997756, lng: -122.066843},
+        population:0,
+        classes: {}
+    },
+    l19: {Location:"Crown Clrm",
+        center: {lat: 36.999611, lng: -122.054966},
+        population: 0,
+        classes: {}
+    },
+    l20: {Location:"Steven Lib",
+        center: {lat: 36.996545, lng: -122.052064},
+        population: 0,
+        classes: {}
+    },
+
+    l21: {Location:"Merrill",
+        center: {lat: 36.999643, lng: -122.053094},
+        population:0,
+        classes: {}
+    },
+    l22: {Location:"R Carson Acad",
+        center: {lat: 36.991187, lng: -122.064671},
+        population: 0,
+        classes: {}
+    },
+    l23: {Location:"KresgeAnnex",
+        center: {lat: 36.997192, lng: -122.065503},
+        population: 0,
+        classes: {}
+    },
+    l24: {Location:"ClassroomUnit",
+        center: {lat: 36.998125, lng: -122.056844},
+        population: 0,
+        classes: {}
+    },
+
+    l25: {Location:"Thim Lecture",
+        center: {lat: 36.998242, lng: -122.061391},
+        population: 0,
+        classes: {}
+    },
+    l26: {Location:"Media Theater",
+        center: {lat: 36.995135, lng: -122.062121},
+        population: 0,
+        classes: {}
+    },
+    l27: {Location:"OPERS Multi Purpose",
+        center: {lat: 36.994902, lng: -122.055000},
+        population: 0,
+        classes: {}
+    },
+
+    l28: {Location:"J Bask Aud",
+        center: {lat: 37.000413, lng: -122.062368},
+        population: 0,
+    },
+    l29: {Location:"BiomedSci",
+        center: {lat: 36.999999, lng: -122.061097},
+        population: 0,
+        classes: {}
+    },
+    l30: {Location:"Thimann Lab",
+        center: {lat: 36.998331, lng: -122.062111},
+        population: 0,
+        classes: {}
+    },
+
+    l31: {Location:"Namaste Lounge",
+        center: {lat: 37.000903, lng: -122.057060},
+        population: 0,
+        classes: {}
+    },
+
+    l32: {Location:"Humn Lecture",
+        center: {lat: 36.998627, lng: -122.054567},
+        population: 0,
+        classes: {}
+    },
+
+    l33: {Location:"Cowell Press",
+        center: {lat: 36.995977, lng: -122.054825},
+        population: 0,
+        classes: {}
+    },
+
+    l34: {Location:"Cowell Com",
+        center: {lat: 36.997149, lng: -122.055141},
+        population: 0,
+        classes: {}
+    },
+
+    l35: {Location:"MingOng Cmp Lb",
+        center: {lat: 36.999961, lng: -122.052371},
+        population: 0,
+        classes: {}
+    },
+
+    l36: {Location:"ISB",
+        center: {lat: 36.998862, lng: -122.059776},
+        population: 0,
+        classes: {}
+    },
+
+    l37: {Location:"Nat Sci",
+        center: {lat: 36.998776, lng: -122.060489},
+        population: 0,
+        classes: {}
+    },
+
+    l38: {Location:"Nat Sci 2",
+        center: {lat: 36.998691, lng: -122.060521},
+        population: 0,
+        classes: {}
+    },
+    l39: {Location:"Agroecology",
+        center: {lat: 37.000557, lng: -122.057416},
+        population: 0,
+        classes: {}
+    },
+    l40: {Location:"Barn Theater",
+        center: {lat: 36.978831, lng: -122.054099},
+        population: 0,
+        classes: {}
+    },
+    l41: {Location:"Music Center",
+        center: {lat: 36.993403, lng: -122.059971},
+        population: 0,
+        classes: {}
+    },
+
+    l42: {Location:"50 Mtr Pool",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l43: {Location:"OPERS Conference",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l44: {Location:"East Gym",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l45: {Location:"E Racquet Ct",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l46: {Location:"E Tennis Ct",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l47: {Location:"E Racquet Ct",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l48: {Location: "Dance Studio",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l49: {Location:"East Field",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l50: {Location:"Fitness/Wellness",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+    l51: {Location:"Martial Arts",
+        center: {lat: 36.994239, lng: -122.055003},
+        population: 0,
+        classes: {}
+    },
+
+    l52: {Location:"Harbor",
+        center: {lat: 36.970849, lng: -122.004431},
+        population: 0,
+        classes: {}
+    },
+    l53: {Location:"West Gym",
+        center: {lat: 36.992409, lng: -122.063963},
+        population: 0,
+        classes: {}
+    },
+    l54: {Location:"Lg Discovery",
+        center: {lat: 36.951352, lng: -122.065101},
+        population: 0,
+        classes: {}
+    },
+    l55: {Location:"TA Foundry",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l56: {Location:"TA Offices",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l57: {Location:"TA 2nd Stage",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l58: {Location:"TA Mainstage",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l59: {Location:"TA Lecture",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l60: {Location:"TA Studio",
+        center: {lat: 36.994864, lng: -122.062143},
+        population: 0,
+        classes: {}
+    },
+    l61: {Location:"Humn Lecture Hall",
+        center: {lat: 36.998627, lng: -122.054567},
+        population: 0,
+        classes: {}
+    },
+    l62: {Location:"Steven Acad",
+        center: {lat: 36.996545, lng: -122.052064},
+        population: 0,
+        classes: {}
+    },
+    L63: {Location:"Cowell Clrm",
+        center: {lat: 36.996658, lng: -122.054161},
+        population: 0,
+        classes: {}
+    },
+    L64: {Location: "J Baskin Engr",
+        center: {lat: 37.000355, lng: -122.063163},
+        population: 0,
+        classes: {}
+    },
+    L65: {Location:"Bay Tree Conf",
+        center: {lat: 36.997959, lng: -122.055591},
+        population: 0,
+        classes: {}
+    }
+
+};
+ //SiliconValleyCtr
